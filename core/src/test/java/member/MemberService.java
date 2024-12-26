de7ecaec7a4992eb04f33ffd1fe6300e8c33b3f8
tejava7177 <wngmstla@sch.ac.kr>
2024-12-26 13:58:14 +0900

    Section 5 스프링 빈 데이터 조회

---
+++ core/src/test/java/member/MemberService.java
@@ -0,0 +1,9 @@
+package member;
+
+public interface MemberService {
+    //가입
+    void join(Member memebr);
+
+    //조회
+    Member findMember(Long meberId);
+}
---
+++ core/src/test/java/member/MemberServiceImpl.java
@@ -0,0 +1,24 @@
+package member;
+
+public class MemberServiceImpl implements MemberService{
+
+    // MemberRepository 라는 인터페이스에 MemoryMemberRepository라는 구현체를 선택
+    // 현재 문제점 : 추상화에도 의존하고 구체화에도 의존하고 있음 = DIP 위반
+    private final MemberRepository memberRepository;
+
+
+    //생성자 주입
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member memebr) {
+        memberRepository.save(memebr);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
